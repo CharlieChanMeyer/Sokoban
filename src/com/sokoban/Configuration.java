ef05ef22be9cf8cd8e4e751d6da29a579d2179a5
Sylvain Jalbert <jalbertsyl@eisti.fr>
2020-05-19 15:06:26 +0200

    Definition de certaines méthodes de la class Configuration

--- src/com/sokoban/Configuration.java
+++ src/com/sokoban/Configuration.java
@@ -6,56 +6,62 @@ import java.util.*;
  * 
  */
 public class Configuration {
-    public ArrayList<Diamant> diamants;
-    public Joueur joueur;
-    public Niveau niveau;
+    private ArrayList<Diamant> diamants;
+    private Joueur joueur;
+    private Niveau niveau;
 
     /**
-     * @param Niveau 
+     * @param niv 
      * @param positionJoueur
      */
     public Configuration(Niveau niv, Position positionJoueur) {
-        // TODO implement here
+        this.niveau = niv;
+        this.diamants = new ArrayList<Diamant>();
+        this.joueur = new Joueur(this, positionJoueur);
     }
 
     /**
-     * @param Configuration
+     * @param config
      */
     public Configuration(Configuration config) {
-        // TODO implement here
+    	this.diamants = config.getDiamants();
+    	this.joueur = config.getJoueur();
+    	this.niveau = config.getNiveau();
     }
 
     /**
-     * @param Position 
+     * @param pos 
      * @return
      */
     public boolean addDiamant(Position pos) {
-        // TODO implement here
-        return false;
+    	boolean valRetour; //la valeur de retour qui indique si tout c'est bien passé
+    	//on suppose que tout se passera bien
+    	valRetour = true;
+    	
+    	try {
+    		this.getDiamants().add(new Diamant(this, pos));
+    	}
+        catch (IndexOutOfBoundsException e) {
+        	//une erreur est survenu, on retourne false
+        	valRetour = false;
+        }
+        return valRetour;
     }
 
     /**
      * @return
      */
     public Integer getX() {
-        // TODO implement here
-        return null;
+        // retourner la valeur X de la position du joueur
+        return this.getJoueur().getPosition().getX();
     }
 
     /**
      * @return
      */
     public Integer getY() {
-        // TODO implement here
-        return null;
-    }
-
-    /**
-     * @return
-     */
-    public Niveau getNiveau() {
-        // TODO implement here
-        return null;
+    	// retourner la valeur Y de la position du joueur
+        return this.getJoueur().getPosition().getY();
     }
 
     /**
@@ -68,27 +74,10 @@ public class Configuration {
     }
 
     /**
-     * @return
-     */
-    public Joueur getJoueur() {
-        // TODO implement here
-        return null;
-    }
-
-    /**
-     * @return
-     */
-    public ArrayList<Diamant> getDiamants() {
-        // TODO implement here
-        return null;
-    }
-
-    /**
      * @param Position 
      * @return
      */
     public boolean estVide(Position pos) {
-        // TODO implement here
         return false;
     }
 
@@ -97,7 +86,6 @@ public class Configuration {
      * @return
      */
     public boolean estCible(Position pos) {
-        // TODO implement here
         return false;
     }
 
@@ -106,16 +94,39 @@ public class Configuration {
      * @return
      */
     public boolean bougerJoueurVers(Direction direction) {
-        // TODO implement here
-        return false;
+    	boolean valRetour; //la valeur de retour qui indique si tout c'est bien passé
+    	//on suppose que tout se passera bien
+    	valRetour = true;
+    	
+    	Position pos = new Position(this.getJoueur().getPosition().getX()+direction.getDx(), this.getJoueur().getPosition().getY()+direction.getDy());
+    	
+    	try {
+    		this.getJoueur().setPosition(pos);
+    	}
+        catch (IndexOutOfBoundsException e) {
+        	//une erreur est survenu, on retourne false
+        	valRetour = false;
+        }
+        return valRetour;
     }
 
     /**
      * @return
      */
     public boolean victoire() {
-        // TODO implement here
         return false;
     }
+
+	public ArrayList<Diamant> getDiamants() {
+		return diamants;
+	}
+
+	public Joueur getJoueur() {
+		return joueur;
+	}
+
+	public Niveau getNiveau() {
+		return niveau;
+	}
 
 }
--- src/com/sokoban/Position.java
+++ src/com/sokoban/Position.java
@@ -9,12 +9,12 @@ public class Position {
     /**
      * 
      */
-    public int x;
+    private int x;
 
     /**
      * 
      */
-    public int y;
+    private int y;
 
     /**
      * @param x 
@@ -59,5 +59,21 @@ public class Position {
         // TODO implement here
         return false;
     }
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
 
 }
