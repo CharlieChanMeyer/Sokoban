466ac01aae3f107595ac3adfd299e4af63e7fbda
Charlie Chan Meyer <charlie.chan.meyer@eisti.fr>
2020-05-22 01:37:13 +0200

    Debut de creation de la fenetre d'affichage du niveau

---
+++ src/com/sokoban/app/Niveau.java
@@ -0,0 +1,17 @@
+package com.sokoban.app;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Niveau extends Application{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+	}
+
+}
---
+++ src/com/sokoban/app/NiveauController.java
@@ -0,0 +1,21 @@
+package com.sokoban.app;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class NiveauController  implements Initializable {
+	
+	private String niveau;
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+	}
+	
+	public void setNiveau(String selecNiveau) {
+		this.niveau = selecNiveau;
+	}
+
+}
